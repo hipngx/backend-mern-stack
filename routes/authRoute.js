8dfb5c05f4fbbefc5739d66c6738175985df60f5
hipngx <19520536@gm.uit.edu.vn>
2022-03-18 17:41:21 +0700

    End backend

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {login, register}=require('../controllers/authController');
+const { login, register, getCurrentUser } = require('../controllers/authController');
+const { checkCurrentUser } = require('../middleware/checkCurrentUser');
 const Route = express.Router();
 
 Route.route('/register').post(register);
 Route.route('/login').post(login);
+Route.route('/').get(checkCurrentUser, getCurrentUser);
 
 module.exports = Route;
